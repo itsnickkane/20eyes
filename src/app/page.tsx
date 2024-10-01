e108dec48dafab4ee5bcbdf8912626d21127384a
Nick Kane <nicholas.r.kane@gmail.com>
2024-09-30 20:44:13 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,8 @@ export default function Page() {
     <div className="flex h-full w-96 max-w-full flex-col px-1 md:w-[1008px]">
       <section className="mt-6 mb-6 flex w-full flex-col md:flex-row">
         <div className="flex w-full flex-row items-center justify-between gap-2 md:gap-0">
-          <a
+         <a>20👀</a>
+           <a
             href={ONCHAINKIT_LINK}
             title="onchainkit"
             target="_blank"
