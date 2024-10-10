cf96f6c9eb574c6744ae1d7c7549fb21626ee9e6
Nick Kane <nicholas.r.kane@gmail.com>
2024-10-10 08:23:38 -0700

    Update page.tsx
    added item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,13 +30,8 @@ export default function Page() {
         </div>
       </section>
       <section className="templateSection flex w-full flex-col items-center justify-center gap-4 rounded-xl bg-gray-100 px-2 py-4 md:grow">
-        <div className="flex h-[450px] w-[450px] max-w-full items-center justify-center rounded-xl bg-[#030712]">
-          <div className="rounded-xl bg-[#F3F4F6] px-4 py-[11px]">
-            <p className="font-normal text-indigo-600 text-xl not-italic tracking-[-1.2px]">
-              npm install @coinbase/onchainkit
-            </p>
-          </div>
-        </div>
+        <div className="flex h-[450px] w-[450px] max-w-full items-center justify-center rounded-xl bg-[#030712]"></div>
+        <div className="flex h-[450px] w-[450px] max-w-full items-center justify-center rounded-xl bg-[#453879]"></div>
         {address ? (
           <TransactionWrapper address={address} />
         ) : (
