a0c1029baa8fc0001568b1869058e6ca3b9d1c78
Nick Kane <nicholas.r.kane@gmail.com>
2024-10-13 15:04:11 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Page() {
         </div>
       </section> 
       <section className="templateSection flex w-full flex-col items-center justify-center gap-4 rounded-xl bg-[#ffed00]px-2 py-4 md:grow">
-        <div className="flex h-[450px] w-[450px] max-w-full items-center justify-center rounded-xl bg-[#030712]">
+        <div className="flex h-[450px] w-[450px] max-w-full items-center justify-center rounded-xl bg-[#ffed00]">
           <div className="rounded-xl bg-[#F3F4F6] px-4 py-[11px]">
             <p className="font-normal text-indigo-600 text-xl not-italic tracking-[-1.2px]">
               npm install @coinbase/onchainkit
