d21caf98fbced7d5c4720aee6c24f76fd7c96ad8
Nick Kane <nicholas.r.kane@gmail.com>
2024-10-13 14:59:11 -0400

    Update page.tsx
    Change background color to yellow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,7 @@ export default function Page() {
           </div>
         </div>
       </section> 
-      <section className="templateSection flex w-full flex-col items-center justify-center gap-4 rounded-xl bg-gray-100 px-2 py-4 md:grow">
+      <section className="templateSection flex w-full flex-col items-center justify-center gap-4 rounded-xl bg-[#ffed00]px-2 py-4 md:grow">
         <div className="flex h-[450px] w-[450px] max-w-full items-center justify-center rounded-xl bg-[#030712]">
           <div className="rounded-xl bg-[#F3F4F6] px-4 py-[11px]">
             <p className="font-normal text-indigo-600 text-xl not-italic tracking-[-1.2px]">
